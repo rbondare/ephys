01990c1be1e13f937863b65e5d168a9f4b4bf015
Rimma Bondarenko <119871740+rbondare@users.noreply.github.com>
2025-07-14 15:58:00 +0200

    Add files via upload

--- ephys_heatmap_spikes.py
+++ ephys_heatmap_spikes.py
@@ -1,29 +1,27 @@
 
 import numpy as np 
-import matplotlib
 import matplotlib.pyplot as plt
-from matplotlib import colors 
 import pyabf 
 import seaborn as sns
 import os
 
-
-
 base_dir = "/Volumes/joeschgrp/Group Members/Rima/Ephys_NE/DATA"
 
-abf1 = pyabf.ABF(os.path.join(base_dir, "30.01.2025 M3/2025_01_30_0013.abf"))
-abf2 = pyabf.ABF(os.path.join(base_dir, "30.01.2025 M3/2025_01_30_0014.abf"))
-abf3 = pyabf.ABF(os.path.join(base_dir, "30.01.2025 M3/2025_01_30_0015.abf"))
-#abf4 = pyabf.ABF(os.path.join(base_dir, "30.01.2025 M3/2025_01_30_0009.abf"))
+abf1 = pyabf.ABF(os.path.join(base_dir, "12.05.2025 M1/2025_05_12_0006.abf"))
+abf2 = pyabf.ABF(os.path.join(base_dir, "12.05.2025 M1/2025_05_12_0007.abf"))
+abf3 = pyabf.ABF(os.path.join(base_dir, "12.05.2025 M1/2025_05_12_0008.abf"))
+#abf4 = pyabf.ABF(os.path.join(base_dir, "13.05.2025 M3/2025_05_13_0017.abf"))
+#abf5 = pyabf.ABF(os.path.join(base_dir, "13.05.2025 M3/2025_05_13_0018.abf"))
 all_sweeps = []
 
 opto_stim = 0.5 
 baseline = 0
-NA_start = 16
-wash = 55
+NA_start = 20
+wash = 60
 
-xlim = [0.48, 0.54]
-#xlim = [0.45, 0.6] 
+#xlim = [0.48, 0.54]
+xlim = [0.45, 0.6] #better for double spikes 
+
 
 for abf in [abf1, abf2, abf3]:
     for sweep in abf.sweepList:
@@ -43,20 +41,17 @@ sweep_data = np.zeros((num_sweeps, num_timepoints))
 for i, sweep in enumerate(all_sweeps):
     sweep_data[i, :] = sweep  # Each row is a sweep, each column is a time point
 
-
-divnorm = colors.TwoSlopeNorm(vmin=-70., vcenter=0, vmax=40)
-
 # Plot heatmap using imshow (better for large data)
 plt.figure(figsize=(10, 6))
-plt.imshow(sweep_data, aspect='auto', cmap='RdBu_r', norm = divnorm, origin='lower',
+plt.imshow(sweep_data, aspect='auto', cmap='gray', origin='lower',
            extent=[abf1.sweepX[0], abf1.sweepX[-1], 0, num_sweeps])
 
 # Labels and Titles
-#plt.axvline(x=opto_stim, color='red', linewidth=2)
-plt.axhline(y=NA_start, color='white', linewidth=2, label="NA start")
-plt.axhline(y=wash, color='white', linewidth=2, label="wash")
-plt.colorbar(label="Voltage (mV)")
-plt.xlabel("Time (s)")
+plt.axvline(x=opto_stim, color='red', linewidth=0.5)
+plt.axhline(y=NA_start, color='white', linewidth=1, label="NA start")
+plt.axhline(y=wash, color='white', linewidth=1, label="wash")
+colorbar = plt.colorbar(label="Voltage (mV)")
+colorbar.mappable.set_clim(-70, 40)  
 plt.ylabel("Sweep Number")
 plt.gca().invert_yaxis()  # Invert the y-axis to make Sweep 0 appear at the top
 plt.xlim(xlim)
