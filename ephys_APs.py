b6eacc264277699a89e346f9ace300d51dab2038
rbondare <rimmabondarenkoo@gmail.com>
2026-04-28 17:29:50 +0200

    notebook for double_opto experiments added

--- ephys_APs.py
+++ ephys_APs.py
@@ -19,10 +19,11 @@ import seaborn as sns
 import os
 import math 
 
-base_dir = "Z:\Group Members\Rima\Ephys_NE\DATA\Opto_Cohort1"
+#base_dir = "Z:\Group Members\Rima\Ephys_NE\DATA\Opto_Cohort1"
+base_dir = "/Volumes/joeschgrp/Group Members/Rima/Ephys_NE/DATA/double_opto/19.03.2026_control"
 
 #abf = pyabf.ABF(os.path.join(base_dir, "30.01.2025 M3/2025_01_30_0019.abf"))
-abf = pyabf.ABF(os.path.join(base_dir, "12.12.2025 M2\\2025_12_12_0010.abf"))
+abf = pyabf.ABF(os.path.join(base_dir, "19.03.2026 M2/2026_03_19_0003.abf"))
 
 #xlim = [0.45, 0.6] #for subplots of 500 ms opto stim 
 #xlim = [0.03, 0.15] #for subplots of 80 ms opto stim 
@@ -45,7 +46,7 @@ for i, sweep in enumerate(abf.sweepList):
     axs[i].plot(abf.sweepX, abf.sweepY, color='black', linewidth=0.6)
     axs[i].set_title(f"Sweep {sweep}", fontsize=5)
     #axs[i].set_xlim(xlim)
-    #axs[i].set_ylim(ylim)S
+    #axs[i].set_ylim(ylim)
     axs[i].tick_params(labelbottom=True, labelsize=4)
 
 # Hide unused subplots
